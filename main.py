793aeaa479d34872c2d7e8e1748cff1371e79dc6
damnoxygen <oxygenilya@mail.com>
2025-03-24 19:19:38 +0200

    sex

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ import sqlite3
 TOKEN = '7663452669:AAHDu1u6bcE8kHk62G_ra8NXCZ-gqYi7K0I'
 bot = telebot.TeleBot(TOKEN, parse_mode='Markdown')
 
-TRIGGER_WORDS = {"тип", "типнуть", "похвала", "похвалить"}
-
+TRIGGER_WORDS = {"тип", "типнуть", "похвала", "похвалить", "типните"}
 
 conn = sqlite3.connect('praise.db', check_same_thread=False)
 cursor = conn.cursor()
